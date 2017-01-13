697c1c7a5fd3acd417727090788de10e5eaa237d
Felix <rumsgr@gmail.com>
2017-01-14 00:10:13 +0800

    加入角色移動功能

---
+++ Assets/Script/Door.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+    public bool doorLock;
+    public bool doorHindden;
+    public Superman man;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Script/Superman.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Superman : MonoBehaviour
+{
+    private GameObject target;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target != null)
+        {
+            this.transform.position = Vector3.MoveTowards(this.transform.position, target.transform.position, Time.deltaTime * 200);
+        }
+    }
+
+    public void Move(GameObject obj)
+    {
+        target = obj;
+    }
+}
